bfc2578ef11f2fa64348013491caee5e29e34999
mkchungs <17832330+mkchungs@users.noreply.github.com>
2022-08-08 11:44:33 -0700

    bring back abiManager

---
+++ substrate/abiManager.js
@@ -0,0 +1,201 @@
+const PolkaholicDB = require("./polkaholicDB");
+const mysql = require("mysql2");
+var SqlString = require('sqlstring');
+const util = require('util');
+// Uses Ankr API to crawl blocks for ETH style chains to store blocks and txs in the BASEDIR
+const exec = util.promisify(require('child_process').exec);
+
+const ethTool = require("./ethTool");
+
+// Function: mint(address poolToken,uint256 amount,address to,uint256 deadline)
+// MethodID: 0x3c173a4f
+// [0]:  000000000000000000000000e69c2c761931c4bf719cf5931af37c6a09889d06
+// [1]:  0000000000000000000000000000000000000000000000022b1c311f1725b308
+// [2]:  000000000000000000000000b554b9856dfdbf52b98e0e4d2b981c34e20e1dab
+// [3]:  00000000000000000000000000000000000000000000000000000000617c5b7d
+const Web3 = require('web3')
+const web3 = new Web3();
+const {
+    BigQuery
+} = require('@google-cloud/bigquery');
+
+function generateMethodOutput(methodString) {
+    var methodInputs = [];
+    var arr = methodString.split(",");
+    arr.forEach(function(item) {
+        var arr = item.trim().split(" ");
+        methodInputs.push({
+            type: arr[0],
+            name: arr[1]
+        });
+    });
+    return methodInputs
+}
+
+function parseTxnByFuncInput(rawMethodString, rawTxHex) {
+    var methodString = rawMethodString.match(/\((.*)\)/).pop();
+    var methodInputs = generateMethodOutput(methodString);
+    var txHex = rawTxHex.substring(10);
+    var res = web3.eth.abi.decodeParameters(methodInputs, txHex);
+    var jsonStr = JSON.stringify(res);
+    return jsonStr
+}
+
+function parseTxnByAbi(abiStr, rawTxHex) {
+    var contractABI = JSON.parse(abiStr)
+    const abiDecoder = require('abi-decoder');
+    abiDecoder.addABI(contractABI);
+    const decodedData = abiDecoder.decodeMethod(rawTxHex);
+    return decodedData
+}
+
+//mint(address poolToken, uint256 amount, address to, uint256 deadline)
+function parseMethodSigniture(abiStr) {
+    var contractABI = JSON.parse(abiStr)
+    //console.log(`${contractABI}`);
+
+    // Concatenate function name with its param types
+    const prepareData = e => `${e.name}(${e.inputs.map(e => e.type)})`
+
+    // Concatenate function name with its param types + inputName
+    const prepareData2 = e => `${e.name}(${e.inputs.map(e =>(e.type + " "+ e.name))})`
+
+
+    // Encode function selector (assume web3 is globally available)
+    const encodeSelector = f => web3.utils.sha3(f).slice(0, 10)
+
+    // Parse ABI and encode its functions
+    //var output = contractABI.filter(e => e.type === "function").flatMap(e => `${encodeSelector(prepareData(e))}|${prepareData2(e)}`)
+    var output = contractABI.filter(e => e.type === "function")
+    var output2 = []
+    output.forEach(function(e) {
+        var methodID = encodeSelector(prepareData(e))
+        var sigature = prepareData2(e)
+        output2.push({
+            methodID: methodID,
+            signature: sigature,
+            abi: [e]
+        })
+    });
+    //var jsonStr = JSON.stringify(output2);
+    //console.log(`${jsonStr}`);
+    return output2
+}
+
+function shexdec(inp) {
+    return parseInt(inp.replace("0x", ""), 16);
+}
+
+function dechex(number) {
+    return parseInt(number, 10).toString(16)
+}
+
+
+// etherscan
+const apiToken = "";
+
+// create table method ( methodID varchar(64), abi blob, signature blob, numContracts int default 0, primary key (methodID))
+// create table address ( address varchar(67), abiRaw mediumblob, numTransactions int default 0, primary key (address) )
+// CREATE TABLE IF NOT EXISTS contractabi ( name VARCHAR(256), fingerprintID VARCHAR(96) PRIMARY KEY not null, signatureID VARCHAR(70) not null, signatureRaw blob, signature blob, abi blob, abiType VARCHAR(16), numContracts int(11) default 0, topicLength int(11) default 0 );
+
+/*{
+  fingerprint: 'transfer(address,uint256)',
+  fingerprintID: '0xa9059cbb',
+  signatureID: '0xa9059cbb',
+  topicLength: 0,
+  signatureRaw: 'transfer(address,uint256)',
+  signature: 'transfer(address dst, uint256 wad)',
+  name: 'Transfer',
+  abi: '[{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]',
+  abiType: 'function'
+}
+...
+{
+  fingerprint: 'Transfer(index_topic_1 address,index_topic_2 address,uint256)',
+  fingerprintID: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef-3-0xc3a8eb6d',
+  signatureID: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+  topicLength: 3,
+  signatureRaw: 'Transfer(address,address,uint256)',
+  signature: 'Transfer(index_topic_1 address src, index_topic_2 address dst, uint256 wad)',
+  name: 'Transfer',
+  abi: '[{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"}]',
+  abiType: 'event'
+}
+*/
+module.exports = class ABIManager extends PolkaholicDB {
+
+    methodMap = {};
+
+    //mint(address poolToken, uint256 amount, address to, uint256 deadline)
+    async computeMethods() {
+        var pool = mysql.createPool(this.connConfig);
+        // now get a Promise wrapped instance of that pool
+        const promisePool = pool.promise();
+        // query database using promises
+        var [contracts, _] = await promisePool.query("SELECT abiRaw from address where abiRaw is Not Null and abiRaw != 'Contract source code not verified' and length(abiRaw) > 20 limit 5000");
+        promisePool.end()
+        var sigs = {};
+        var numContractsTally = {};
+        for (const c of contracts) {
+            var contractABIStr = c.abiRaw
+            var output = ethTool.parseAbiSignature(contractABIStr)
+            output.forEach(function(e) {
+                var fingerprintID = e.fingerprintID
+                if (!sigs[fingerprintID]) {
+                    sigs[fingerprintID] = e;
+                    numContractsTally[fingerprintID] = 1;
+                } else {
+                    numContractsTally[fingerprintID]++;
+                }
+            });
+        }
+        let abiRows = [];
+        for (const fingerprintID of Object.keys(sigs)) {
+            let data = sigs[fingerprintID];
+            let numContracts = numContractsTally[fingerprintID];
+            //fingerprintID, signatureID, signatureRaw, signature, name, abi ,abiType, numContracts, topicLength
+            let row = `('${data.fingerprintID}', '${data.signatureID}', '${data.signatureRaw}', '${data.signature}', '${data.name}', '${data.abi}', '${data.abiType}', '${numContracts}', '${data.topicLength}')`
+            abiRows.push(row);
+        }
+        console.log(abiRows.length + " records");
+        await this.dump_contract_abis(abiRows)
+        await this.update_batchedSQL(true);
+    }
+
+    async load_abi(contractABIStr) {
+        var sigs = {};
+        var numContractsTally = {};
+        var output = ethTool.parseAbiSignature(contractABIStr)
+        output.forEach(function(e) {
+            var fingerprintID = e.fingerprintID
+            if (!sigs[fingerprintID]) {
+                sigs[fingerprintID] = e;
+                numContractsTally[fingerprintID] = 1;
+            } else {
+                numContractsTally[fingerprintID]++;
+            }
+        });
+        let abiRows = [];
+        for (const fingerprintID of Object.keys(sigs)) {
+            let data = sigs[fingerprintID];
+            let numContracts = numContractsTally[fingerprintID];
+            //fingerprintID, signatureID, signatureRaw, signature, name, abi ,abiType, numContracts, topicLength
+            let row = `('${data.fingerprintID}', '${data.signatureID}', '${data.signatureRaw}', '${data.signature}', '${data.name}', '${data.abi}', '${data.abiType}', '${numContracts}', '${data.topicLength}')`
+            abiRows.push(row);
+        }
+        console.log(abiRows.length + " records");
+        await this.dump_contract_abis(abiRows)
+        await this.update_batchedSQL(true);
+    }
+
+    async dump_contract_abis(abiRows) {
+        let i = 0;
+        for (i = 0; i < abiRows.length; i += 2000) {
+            let j = i + 10000;
+            if (j > abiRows.length) j = abiRows.length;
+            let sql = "insert into contractabi (fingerprintID, signatureID, signatureRaw, signature, name, abi ,abiType, numContracts, topicLength) values " + abiRows.slice(i, j).join(",") + " on duplicate key update name = values(name), signature = values(signature), signatureRaw = values(signatureRaw),signatureID = values(signatureID), abi = values(abi), abiType = values(abiType), numContracts = values(numContracts), topicLength = values(topicLength)";
+            this.batchedSQL.push(sql)
+        }
+        console.log(`dump_contract_abi len=${abiRows.length}`);
+    }
+}
