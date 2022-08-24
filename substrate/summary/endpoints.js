94890b684c40a909d7c55dd86c04aca6dc11021b
Leonardo Custodio <leonardo@enjin.io>
2022-08-24 20:17:01 -0300

    Updates efinity endpoints

--- substrate/summary/endpoints.js
+++ substrate/summary/endpoints.js
@@ -566,9 +566,11 @@ var polkadotEndpoints = [
         info: 'efinity',
         homepage: 'https://efinity.io',
         paraId: 2021,
-        text: t('rpc.polkadot.efinity', 'Efinity', { ns: 'apps-config' }),
+        text: t('archive.rpc.polkadot.efinity', 'Efinity', { ns: 'apps-config' }),
         providers: {
-          Efinity: 'wss://rpc.efinity.io'
+          Efinity: 'wss://archive.rpc.efinity.io',
+          Dwellir: 'wss://efinity-rpc.dwellir.com',
+          OnFinality: 'wss://efinity.api.onfinality.io/public-ws'
         }
       },
       {
