5c789987d70d34f9d82c4377bca0520c04dcb6da
MK <michael@colorfulnotion.com>
2022-09-28 17:32:24 -0700

    buildXcmInfo

--- substrate/xcmmanager.js
+++ substrate/xcmmanager.js
@@ -263,6 +263,125 @@ module.exports = class XCMManager extends Query {
         }, sqlDebug); */
     }
 
+    async buildXcmInfo(substrateTx, matchRec){
+        let decorate = true
+        let decorateExtra = ["usd", "address", "related", "data"]
+        let d = substrateTx
+        let xcm = matchRec
+        let sourceTxFee = d.fee
+        let sourceTxFeeUSD = d.feeUSD
+        let sourceChainSymbol = d.chainSymbol
+        if (d.evm != undefined && d.evm.transactionHash != undefined){
+            let evmtx = await this.getTransaction(d.evm.transactionHash, decorate, decorateExtra, false);
+            sourceTxFee = evmtx.fee
+            sourceTxFeeUSD = evmtx.feeUSD
+            sourceChainSymbol = evmtx.symbol
+        }
+
+        console.log(`sourceTxFee=${sourceTxFee}, sourceTxFeeUS=${sourceTxFeeUSD}, sourceChainSymbol=${sourceChainSymbol}`)
+        console.log(`rawXCM`, xcm)
+        xcm.chainIDName = this.getChainName(xcm.chainID);
+        xcm.chainIDDestName = this.getChainName(xcm.chainIDDest);
+        let chainIDDestInfo = this.chainInfos[xcm.chainIDDest]
+        if (xcm.chainIDDest != undefined && chainIDDestInfo != undefined && chainIDDestInfo.ss58Format != undefined) {
+            if (xcm.destAddress != undefined) {
+                if (xcm.destAddress.length == 42) xcm.destAddress = xcm.destAddress
+                if (xcm.destAddress.length == 66) xcm.destAddress = paraTool.getAddress(xcm.destAddress, chainIDDestInfo.ss58Format)
+            } else if (xcm.fromAddress != undefined) {
+                if (xcm.fromAddress.length == 42) xcm.destAddress = xcm.fromAddress
+                if (xcm.fromAddress.length == 66) xcm.destAddress = paraTool.getAddress(xcm.fromAddress, chainIDDestInfo.ss58Format)
+            }
+        }
+        if (d.signer != undefined) {
+            xcm.fromAddress = d.signer
+        }
+
+        let decimals = false
+        let isNewFormat = true
+        if (xcm.asset != undefined){
+            isNewFormat = false
+        }
+
+        let symbolRelayChain = paraTool.makeAssetChain(xcm.symbol, xcm.relayChain);
+        let xcmAssetInfo = this.getXcmAssetInfoBySymbolKey(symbolRelayChain)
+        if (xcmAssetInfo != undefined && xcmAssetInfo.decimals){
+            decimals = xcmAssetInfo.decimals
+        }
+        if (!isNewFormat){
+            if (decimals === false) {
+                decimals = this.getAssetDecimal(xcm.asset, xcm.chainID)
+            }else if (decimals === false) {
+                decimals = this.getAssetDecimal(xcm.asset, xcm.chainIDDest)
+            }
+            if (decimals !== false){
+                xcm.amountSent = xcm.amountSent / 10 ** decimals;
+                xcm.amountReceived = xcm.amountReceived / 10 ** decimals;
+            }
+            xcm.symbol = this.getAssetSymbol(xcm.asset);
+        }
+
+        //fee -> initiation + teleport fee
+        if (decimals !== false) {
+            xcm.fee = xcm.amountSent - xcm.amountReceived
+            xcm.feeUSD = xcm.fee * (xcm.amountSentUSD / xcm.amountReceived) // temp hack
+            xcm.sourceTxFee = sourceTxFee
+            xcm.sourceTxFeeUSD = sourceTxFeeUSD
+            xcm.sourceChainSymbol = sourceChainSymbol
+
+            /*await this.decorateUSD(xcm, "amountSent", xcm.asset, xcm.chainID, xcm.destTS, decorateUSD)
+            if (decorateUSD){
+              xcm.amountReceivedUSD = xcm.priceUSD * xcm.amountReceived;
+            }
+            */
+            let p = await this.computePriceUSD({
+                val: xcm.amountSent,
+                asset: xcm.asset,
+                chainID: xcm.chainID,
+                ts: xcm.destTS
+            });
+            if (p) {
+                xcm.amountSentUSD = p.valUSD;
+                xcm.amountReceivedUSD = p.priceUSD * xcm.amountReceived;
+                xcm.feeUSD = p.priceUSD * xcm.fee
+                xcm.priceUSD = p.priceUSD;
+                xcm.priceUSDCurrent = p.priceUSDCurrent;
+            }
+        }
+        let xcmInfo = {
+            symbol: xcm.symbol,
+            relayChain: xcm.relayChain,
+            origination: null,
+            destination: null,
+            version: (isNewFormat)? 'V2' : 'V1'
+        }
+        xcmInfo.origination = {
+            chainIDName: xcm.chainIDName,
+            chainID: xcm.chainID,
+            fromAddress: xcm.fromAddress,
+            amountSent: xcm.amountSent,
+            txFee: xcm.sourceTxFee,
+            txFeeUSD: xcm.sourceTxFeeUSD,
+            txFeeSymbol: xcm.sourceChainSymbol,
+            extrinsicID: xcm.extrinsicID,
+            extrinsicHash: xcm.extrinsicHash,
+            msgHash: xcm.msgHash,
+            sourceTS: xcm.sourceTS,
+        }
+        xcmInfo.destination = {
+            chainIDDestName: xcm.chainIDDestName,
+            chainIDDest: xcm.chainIDDest,
+            destAddress: xcm.destAddress,
+            blockNumberDest: xcm.blockNumberDest,
+            amountReceived: xcm.amountReceived,
+            teleportFee: xcm.fee,
+            teleportFeeUSD: xcm.feeUSD,
+            teleportFeeChainSymbol: xcm.symbol,
+            destExtrinsicID: xcm.destExtrinsicID,
+            destEventID: xcm.eventID,
+            destTS: xcm.destTS,
+        }
+        return [xcmInfo, xcm] //TODO: drop xcm format
+}
 
     // xcmtransfer_match matches cross transfers between SENDING events held in "xcmtransfer"  and CANDIDATE destination events (from various xcm received messages on a destination chain)
     // this will be phased out soon
@@ -320,7 +439,7 @@ module.exports = class XCMManager extends Query {
             let matched = {}
             let matches = 0;
             if (xcmmatches.length > 0) {
-                console.log("[Found] match_xcm", sqlA)
+                console.log(`[Found] match_xcm (A) len=${xcmmatches.length}`)
             } else {
                 //enable this for debugging
                 //console.log("[Empty] match_xcm", sqlA)
@@ -378,12 +497,7 @@ module.exports = class XCMManager extends Query {
                     // problem: this does not store the "dust" fee (which has rat << 1) in bigtable
                     // TODO: generate xcmInfo struct
 
-                    let substrateTxHash = d.extrinsicHash
-                    let substratetx = await this.getTransaction(substrateTxHash);
-                    if (substratetx != undefined){
-                        console.log(`extrinsicHash=${substrateTxHash}`, substratetx)
-                    }
-
+                    // we use matchRec to build xcmInfo
                     let match = {
                         chainID: d.chainID,
                         chainIDDest: d.chainIDDest,
@@ -404,6 +518,13 @@ module.exports = class XCMManager extends Query {
                         sourceTS: d.sourceTS,
                         destTS: d.destTS
                     }
+                    let substrateTxHash = d.extrinsicHash
+                    let substratetx = await this.getTransaction(substrateTxHash);
+                    let xcmInfo, xcmOld;
+                    if (substratetx != undefined){
+                        [xcmInfo, xcmOld] = await this.buildXcmInfo(substratetx, match)
+                    }
+                    console.log(`extrinsicHash=${substrateTxHash}, xcmInfo`, xcmInfo)
 
                     // 1. write "addressExtrinsic" feedxcmdest with row key address-extrinsicHash and column extrinsicHash#chainID-extrinsicID-transferIndex
                     //    * we use the SENDING chainID + the sender extrinsicID as the column for the "feedxcmdest" column family
