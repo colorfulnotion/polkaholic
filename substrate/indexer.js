3bbd6421521d63936aa668d64c36d164671f6c86
mkchungs <17832330+mkchungs@users.noreply.github.com>
2022-11-07 09:49:59 -0800

    indexBlocks

--- substrate/indexer.js
+++ substrate/indexer.js
@@ -7157,6 +7157,7 @@ from assetholder${chainID} as assetholder, asset where assetholder.asset = asset
         await this.setup_chainParser(chain, this.debugLevel);
         await this.initApiAtStorageKeys(chain, blockHash, blockNumber);
         this.chainID = chain.chainID;
+        blockNumber = paraTool.dechexToInt(blockNumber)
         //await this.setupChainAndAPI(chainID);
 
         try {
