d10f509db9f356fd846a0c791af91980e63d925c
mkchungs <michael@colorfulnotion.com>
2023-10-16 13:53:56 -0700

    missingBNAll

--- substrate/substrateetl.js
+++ substrate/substrateetl.js
@@ -4644,6 +4644,7 @@ from blocklog join chain on blocklog.chainID = chain.chainID where logDT <= date
         //TEST:
         //bnStart = 17663809
         //bnEnd = 17663810
+        let missingBNAll = [];
         let jmpIdx = 0
         let jmpTotal = Math.ceil((bnEnd - bnStart) / jmp);
         for (let bn0 = bnStart; bn0 <= bnEnd; bn0 += jmp) {
@@ -4658,6 +4659,8 @@ from blocklog join chain on blocklog.chainID = chain.chainID where logDT <= date
             let verifiedRows = res.verifiedRows
             if (missingBNs.length > 0){
                 console.log(`missingBNs:${missingBNs.length}`, missingBNs)
+                missingBNAll.push(...missingBNs)
+                /*
                 const Crawler = require("./crawler");
                 let crawler = new Crawler();
                 await crawler.setupAPI(chain);
@@ -4677,6 +4680,20 @@ from blocklog join chain on blocklog.chainID = chain.chainID where logDT <= date
                     console.log(`Fetch failed missingBN=${missingBNs}, continue`)
                     //process.exit(1, `validate_trace error`)
                 }
+                */
+            }
+            const Crawler = require("./crawler");
+            let crawler = new Crawler();
+            await crawler.setupAPI(chain);
+            await crawler.assetManagerInit();
+            await crawler.setupChainAndAPI(chainID);
+            console.log(`missingBNAll:${missingBNAll.length}`, missingBNAll)
+            for (const targetBN of missingBNAll){
+                let t2 = {
+                    chainID,
+                    blockNumber: targetBN
+                };
+                let x = await crawler.crawl_block_trace(chain, t2);
             }
         }
     }
